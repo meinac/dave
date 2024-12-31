8d926ed55baabed25d30c4c65b6365374d4bcac7
Mehmet Emin INAC <mehmetemininac@gmail.com>
2024-12-31 18:57:43 +0300

    Add router and home page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import { useRouter } from 'vue-router';
+
+  export default {
+    name: 'App',
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+    data: () => ({ drawer: false, group: null }),
+
+    setup() {
+      const router = useRouter();
+      const menuItems = router.options.routes.filter(route => route.name);
+
+      return { menuItems };
+    },
+  };
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-app-bar color="primary" prominent app>
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-start">Diving-related calculations</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      :location="$vuetify.display.mobile ? 'bottom' : undefined"
+      temporary
+      :scrim="false"
+      app>
+      <v-list>
+        <v-list-item
+          v-for="route in menuItems"
+          :key="route.path"
+          :to="route.path"
+          router
+        >
+          <v-list-item-title>{{ route.name }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+    <v-footer app>Developed by Mehmet Emin INAC</v-footer>
+  </v-app>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,20 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+import router from './router/router.js';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const app = createApp(App)
+
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
